75b2462ff16b5901a16ef543f31fb11eca8cd05b
Brent O'Connor <epicserve@gmail.com>
2022-09-28 08:12:17 -0500

    Setup parametrize

--- tests/test_recipes.py
+++ tests/test_recipes.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+import pytest as pytest
 from faker import Faker
 from model_bakery.recipe import Recipe
 
@@ -9,18 +9,26 @@ Tests to try and isolate why the export tests with snapshots don't work after up
 versions less than 1.3.2 work and version greater than 1.3.3 don't work. All the tests fail randomly, so you have to run
 them multiple times to see if they work or don't work.
 
+Facts:
+1. Versions 1.1.1 to 1.3.2 works. Starting with version 1.3.3 it breaks.
+
 Things tried:
 1. Upgrading to the newest version of Faker
 2. Upgrading to Django 4.1.1 fixed it, however upgrading from 3.2.9 to 3.2.15 didn't fix it
 3. Tried upgrading to model-bakery 1.7.0 and that didn't fix it
+4. When using parametrize, they either all pass or all fail
+5. Tried downgrading to pytest 6.2.5 from 7.1.3 and it didn't fix it
 
 Strangely enough try to reproduce the same bug in a fork of model_bakery didn't produce any results.
 See: https://github.com/epicserve/model_bakery/compare/main...epicserve:model_bakery:bug/faker_seeding_bug?expand=1
 """
 
 
-class TestFakerSeeding(TestCase):
-    def test_faker_directly(self):
+@pytest.mark.django_db
+class TestFakerSeeding:
+
+    @pytest.mark.parametrize("num", list(range(1, 11)))
+    def test_faker_directly(self, num):
         """
         WORKS 100% of the time!!!!
         for i in {1..20}; do pytest tests/test_recipes.py::TestFakerSeeding::test_faker_directly; done
@@ -31,7 +39,8 @@ class TestFakerSeeding(TestCase):
         assert username1 == "pattersonbelinda"
         assert username2 == "stevenhenry"
 
-    def test_faker_seeding_from_global(self):
+    @pytest.mark.parametrize("num", list(range(1, 11)))
+    def test_faker_seeding_from_global(self, num):
         """
         for i in {1..20}; do pytest tests/test_recipes.py::TestFakerSeeding::test_faker_seeding_from_global; done
         """
@@ -46,7 +55,8 @@ class TestFakerSeeding(TestCase):
         user = user_recipe.make()
         assert user.username == "rli"
 
-    def test_faker_seeding_form_local(self):
+    @pytest.mark.parametrize("num", list(range(1, 11)))
+    def test_faker_seeding_form_local(self, num):
         """
         for i in {1..20}; do pytest tests/test_recipes.py::TestFakerSeeding::test_faker_seeding_form_local; done
         """
